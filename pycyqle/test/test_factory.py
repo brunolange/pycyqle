7b4fcf882eb8ae8749ea387971f5a7cb2e800251
Bruno Lange <bruno.lange@cern.ch>
2018-07-05 21:18:28 -0700

    Tests factory constructors

--- pycyqle/test/test_factory.py
+++ pycyqle/test/test_factory.py
@@ -2,17 +2,29 @@ import unittest
 from pycyqle.factory import Factory
 
 class FactoryTest(unittest.TestCase):
-    def setUp(self):
-        self.factory = Factory()
-        self.factory\
-            .name('test-factory')\
-            .table('test-table')\
-            .primary_key('id')
 
-    def test_factory_build(self):
-        self.assertEqual(self.factory.name(), 'test-factory')
-        self.assertEqual(self.factory.table(), 'test-table')
-        self.assertEqual(self.factory.primary_key(), 'id')
+    def test_param_build(self):
+        factory = Factory.param_build(
+            name='bicycle-factory',
+            table='bicycle',
+            primary_key='id'
+        )
+        self._assert_bicycle_factory(factory)
+        return factory
+
+    def test_dict_build(self):
+        factory = Factory.dict_build({
+            'name': 'bicycle-factory',
+            'table': 'bicycle',
+            'primary_key': 'id'
+        })
+        self._assert_bicycle_factory(factory)
+        return factory
+
+    def _assert_bicycle_factory(self, factory):
+        self.assertEqual(factory.name(), 'bicycle-factory')
+        self.assertEqual(factory.table(), 'bicycle')
+        self.assertEqual(factory.primary_key(), 'id')
 
 if __name__ == '__main__':
     unittest.main()
