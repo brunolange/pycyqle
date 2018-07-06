fa61e2e1995d224b21f77b5fa26a197f94f2b7b3
Bruno Lange <bruno.lange@cern.ch>
2018-07-05 21:18:56 -0700

    Tests factory queries

--- pycyqle/test/test_factory.py
+++ pycyqle/test/test_factory.py
@@ -1,5 +1,6 @@
 import unittest
-from pycyqle.factory import Factory
+import re
+from pycyqle.factory import Factory, Component
 
 class FactoryTest(unittest.TestCase):
 
@@ -25,6 +26,41 @@ class FactoryTest(unittest.TestCase):
         self.assertEqual(factory.name(), 'bicycle-factory')
         self.assertEqual(factory.table(), 'bicycle')
         self.assertEqual(factory.primary_key(), 'id')
+
+    @staticmethod
+    def _format_query(query):
+        return re.sub('\s?,\s?', ',', ' '.join(query.split()))
+
+    def test_query(self):
+        components = list(map(Component.dict_build, [
+            {'name': 'tire', 'column': 'tire'},
+            {'name': 'seat', 'column': 'seat'}
+        ]))
+        factory = self.test_dict_build()
+        factory.components(components)
+
+        self.assertEqual(len(factory.components()), len(components))
+        self.assertEqual(
+            FactoryTest._format_query(factory.query(['tire'], {})),
+            FactoryTest._format_query("""
+                SELECT bicycle.id AS "__id__"
+                ,   bicycle.tire AS tire
+                FROM bicycle WHERE 1=1
+            """
+        ))
+
+        new_components = list(map(Component.dict_build, [
+            {'name': 'pedal', 'column': 'pedal'}
+        ]))
+        factory.components(components + new_components)
+
+        self.assertEqual(len(factory.components()), len(components) + len(new_components))
+        self.assertEqual(
+            FactoryTest._format_query(factory.query(['seat', 'pedal'], {'id0': 42})),
+            FactoryTest._format_query("""
+            SELECT bicycle.id AS "__id__" , bicycle.seat AS seat, bicycle.pedal AS pedal
+            FROM bicycle WHERE bicycle.id IN (%(id0)s)
+        """))
 
 if __name__ == '__main__':
     unittest.main()
